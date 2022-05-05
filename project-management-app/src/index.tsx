139f15b58d0f78af0ff4d50ed76eef7ae9cac7c7
jenya-b <evgeny_rab@inbox.ru>
2022-05-05 22:34:23 +0300

    feat: implement modal window reducer

--- project-management-app/src/index.tsx
+++ project-management-app/src/index.tsx
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { App } from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { setupStore } from './store/store';
 import './index.scss';
+
+const store = setupStore();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- project-management-app/src/store/store.ts
+++ project-management-app/src/store/store.ts
@@ -1,5 +1,8 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import modalWindowReducer from './reducers/modalWindowSlice';
 
-export const rootReducer = combineReducers({});
+export const rootReducer = combineReducers({
+  modalWindowReducer,
+});
 
 export const setupStore = () => configureStore({ reducer: rootReducer });
