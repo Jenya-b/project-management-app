6b2efe9c8907d948380532751fdde164a1f1232a
jenya-b <evgeny_rab@inbox.ru>
2022-05-10 22:46:34 +0300

    refactor: replace select with checkbox

--- project-management-app/src/modules/components/header/index.tsx
+++ project-management-app/src/modules/components/header/index.tsx
@@ -4,12 +4,11 @@ import { navLinkTitle, settingsProfile } from '../../constants/constHeader';
 import { pathToPage } from '../../constants/constRoutes';
 import AddIcon from '@mui/icons-material/Add';
 import SearchIcon from '@mui/icons-material/Search';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
 import {
   AppBar,
   Avatar,
   Box,
+  FormLabel,
   IconButton,
   InputBase,
   Menu,
@@ -21,28 +20,25 @@ import {
 import MenuIcon from '@mui/icons-material/Menu';
 import './index.scss';
 import { useAppDispatch } from '../../../hooks/useAppDispatch';
-import { useAppSelector } from '../../../hooks/useAppSelector';
 import { langInterfaceSlice } from '../../../store/reducers/langInterfaceSlice';
-import { defaultLanguage, languagesArray } from '../../constants/constUtils';
 import { useTranslation } from 'react-i18next';
 
 export const Header = () => {
   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
   const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+  const [isEnglishLanguage, setIsEnglishLanguage] = useState(true);
   const dispatch = useAppDispatch();
-  const { language } = useAppSelector((state) => state.langInterfaceReducer);
   const { setLanguage } = langInterfaceSlice.actions;
   const { t, i18n } = useTranslation();
 
   useEffect(() => {
-    const lang = localStorage.getItem('i18nextLng');
-    dispatch(setLanguage(lang ?? defaultLanguage));
-  }, []);
+    dispatch(setLanguage(isEnglishLanguage ? 'en' : 'ru'));
+    i18n.changeLanguage(isEnglishLanguage ? 'en' : 'ru');
+  }, [isEnglishLanguage]);
 
-  const handleLanguageChange = (event: SelectChangeEvent) => {
-    const { value } = event.target;
-    dispatch(setLanguage(value));
-    i18n.changeLanguage(value);
+  const handleLanguageChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const { checked } = event.currentTarget;
+    setIsEnglishLanguage(checked);
   };
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
@@ -146,6 +142,18 @@ export const Header = () => {
               <SearchIcon />
             </IconButton>
           </Box>
+          <FormLabel id="language-switcher">
+            <input
+              type="checkbox"
+              className="language-switcher__checkbox"
+              checked={isEnglishLanguage}
+              onChange={handleLanguageChange}
+            />
+
+            <span id="round-slider"></span>
+            <span id="select-ru">RU</span>
+            <span id="select-en">EN</span>
+          </FormLabel>
           <IconButton
             onClick={handleOpenUserMenu}
             sx={{
@@ -199,19 +207,6 @@ export const Header = () => {
               </MenuItem>
             ))}
           </Menu>
-          <FormControl variant="standard" className="multiple-language">
-            <Select
-              value={language}
-              className="multiple-language__select"
-              onChange={handleLanguageChange}
-            >
-              {languagesArray.map((language) => (
-                <MenuItem key={language} value={language}>
-                  {language}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
         </Box>
       </Toolbar>
     </AppBar>
